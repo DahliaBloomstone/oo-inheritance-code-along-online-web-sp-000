1214e1b2cb0989ee1e3b534c22c093dc8b9e2334
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-01 17:22:28 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,4 +1,13 @@
 require_relative "./vehicle.rb"
 #Child class!
 class Car < Vehicle
+def go #overwrite the inherited go method with one specific to Car class.
+"VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
 end 
+end 
+
+
+
+
+#use < to inherit the Car class from Vehicle. 
+#no methods in car class. 
